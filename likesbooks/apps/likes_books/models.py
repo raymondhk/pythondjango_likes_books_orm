1cef87ae2d3e7712a62f1df8ecba8da3fb050240
Raymond Kim <ray.kim09@gmail.com>
2017-09-21 14:39:31 -0400

    Finished Assignment with shell command txt

--- likesbooks/apps/likes_books/models.py
+++ likesbooks/apps/likes_books/models.py
@@ -9,8 +9,14 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
-        return '%s %s %s' (self.first_name, self.last_name, self.email)
+        return '%s %s %s' % (self.first_name, self.last_name, self.email)
 
 class Book(models.Model):
     name = models.CharField(max_length=255)
-    desc
+    desc = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    uploader = models.ForeignKey(User, related_name="uploaded_books")
+    liked_users = models.ManyToManyField(User, related_name="like_books")
+    def __str__(self):
+        return 'Name: %s Desc: %s Uploader: %s' % (self.name, self.desc, self.uploader)
--- likesbooks/likesbooks/settings.py
+++ likesbooks/likesbooks/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.like_books',
+    'apps.likes_books',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
